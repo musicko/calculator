18b137d03c59eb8744d5725ec9a750ad88391c70
KimJinHo <azurepassion@bugs.co.kr>
2017-10-11 18:12:18 +0900

    테스트목적으로 수정합니다.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-////
+﻿////
 #include <stdio.h>
  
 int add(int, int);
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 	int number1, number2, result;
 	char operator;
 	
-	printf("Please enter a expression");
+	printf("Please enter a expressionsssssss");
 	scanf("%d %c %d", &number1, &operator, &number2);
 	
 	switch (operator)
